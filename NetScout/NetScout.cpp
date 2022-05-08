6b9fcf9a1b954aa99a5d3a2ebb5630aec1d8761d
Gergő Szabó <lest12110@gmail.com>
2022-05-08 13:21:08 +0200

    fixed bugs advancements

--- NetScout/NetScout.cpp
+++ NetScout/NetScout.cpp
@@ -9,17 +9,36 @@
 #include <chrono>
 #include <vector>
 #include <regex>
+#include <sstream>
+#include <Windows.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 void writepingfile()
 {
+    std::filesystem::remove_all("C:\\Programs\\netdet\\reports");
     std::ofstream pingfile;
-    _mkdir("c:\\Programs\\netdet");
+    _mkdir("C:\\Programs\\netdet");
     _mkdir("C:\\Programs\\netdet\\reports");
     pingfile.open("C:\\Programs\\netdet\\ping.cmd");
-    pingfile << "ping %1 -a -n 5 > C:\\Programs\\netdet\\reports\\%1.report\nexit";
+    pingfile << "ping %1 -a -n 5 > C:\\Programs\\netdet\\reports\\%1.report\n\"done\" > C:\\Programs\\netdet\\reports\\%1.done\nexit";
     pingfile.close();
 }
 
+std::vector<std::string> readlines(std::string path) {
+    std::vector<std::string> lines;
+    std::ifstream file(path);
+    if (file.is_open())
+    {
+        std::string line;
+        while (std::getline(file, line))
+        {
+            lines.push_back(line);
+        }
+    }
+    return(lines);
+}
 
 std::vector<std::string> ssplit(std::string text, std::string delim)
 {
@@ -52,27 +71,104 @@ std::string getdefgw()
     return(dat);
 }
 
-void ping(std::string host)
+
+void ping(std::string host, int thrid)
 {
     std::string pipa = "start C:\\Programs\\netdet\\ping.cmd " + host;
     system(pipa.c_str());
-
 }
-
+//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//MAIN//
+static int iprange = 256;
+static int threadcount = 25;
+std::vector<std::string> fps;
 int main()
 {
-    std::string gateway = getdefgw();
     writepingfile();
+    std::string gateway = getdefgw();
+    std::string ip;
     std::vector<std::thread> pingsvect;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < iprange; i++)
     {
-        std::string ip = gateway + std::to_string(i);
-        pingsvect.push_back(std::thread(ping, ip));
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
+        ip = gateway + std::to_string(i - threadcount);
+        std::ifstream f(("C:\\Programs\\netdet\\reports\\" + ip.substr(1, ip.length()) + ".done").c_str());
+        std::cout << std::endl;
+        while (i > threadcount)
+        {
+            f.close();
+            f.open(("C:\\Programs\\netdet\\reports\\" + ip.substr(1, ip.length()) + ".done").c_str());
+            std::cout << "\rC:\\Programs\\netdet\\reports\\" + ip.substr(1, ip.length()) + ".done";
+            if (f.good()) {
+                std::cout << std::endl;
+                break;
+            }
+        }
+        f.close();
+        ip = gateway + std::to_string(i);
+        pingsvect.push_back(std::thread(ping, ip, i));
         pingsvect[i].join();
     }
+    ip = gateway + std::to_string(iprange - 1);
 
+    while (true) {
+        std::ifstream f(("C:\\Programs\\netdet\\reports\\" + ip.substr(1, ip.length()) + ".done").c_str());
+        std::cout << "\rC:\\Programs\\netdet\\reports\\" + ip.substr(1, ip.length()) + ".done";
+        if (f.good()) {
+            std::cout << std::endl;
+            f.close();
+            break;
+        }
+        f.close();
+    }
+
+    for (int i = 0; i < iprange; i++)
+    {
+        std::string ip = gateway + std::to_string(i);
+        std::string path = "C:\\Programs\\netdet\\reports\\" + ip.erase(0, 1) + ".report";
+        //std::cout << path << std::endl;
+        std::vector<std::string> thisf = readlines(path);
+        std::string thisl = thisf[9];
+        std::vector<std::string> thisls = ssplit(thisl, "=");
+        std::string avgrpas = "";
+        int avgrpai = 0;
+        try
+        {
+            try
+            {
+                if (thisf.size() > 10) {
+                    std::string avgrp = ssplit(thisf[thisf.size() - 1], "=")[2];
+                    //std::cout << avgrp << std::endl;
+                    for (int j = 0; j < avgrp.length(); j++)
+                    {
+                        if (isdigit(avgrp[j]))
+                        {
+                           // std::cout << avgrp[j];
+                            avgrpas += avgrp[j];
+                            avgrpai = stoi(avgrpas);
+                        }
+                    }
+                }
+            }
+            catch (const std::exception& e) 
+            {
+                std::cout << "error occured: " << e.what() << std::endl;
+                std::cout << avgrpas;
+                return false;
+            }
+            int sent = std::stoi(&thisls[1][1]);
+            int rece = std::stoi(&thisls[2][1]);
+            if (avgrpai > 0)
+            {
+                std::cout << gateway + std::to_string(i) << ": \n\t" << rece << "/" << sent << " packages retreeived\n\t avg response time: " << avgrpai;
+                std::cout << std::endl;
+            }
+
+        }
+        catch (const std::exception& e) 
+        {
+            std::cout << "error occured: " << e.what() << std::endl;
+            std::cout << avgrpas;
+            return false;
+        }
+
+   }
 }
